d448250bc1d4f4d9279b4a6616b6a91ed8ca0ced
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-02-16 19:34:10 +0300

    Added MorphologicalTag to distribution.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     ext_modules=cythonize(["MorphologicalAnalysis/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-MorphologicalAnalysis-Cy',
-    version='1.0.9',
+    version='1.0.10',
     packages=['MorphologicalAnalysis'],
     package_data={'MorphologicalAnalysis': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/TurkishMorphologicalAnalysis-Cy',

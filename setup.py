698260d0a31a6b468c1613d7df9a0eabd6a456a7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 21:47:58 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["MorphologicalAnalysis/*.pyx", "DisambiguationCorpus/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-MorphologicalAnalysis-Cy',
-    version='1.0.17',
+    version='1.0.18',
     packages=['MorphologicalAnalysis', 'DisambiguationCorpus'],
     package_data={'MorphologicalAnalysis': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'DisambiguationCorpus': ['*.pxd', '*.pyx', '*.c', '*.py']},
@@ -14,5 +18,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Morphological Analysis',
-    install_requires=['NlpToolkit-Dictionary-Cy', 'NlpToolkit-Corpus-Cy', 'NlpToolkit-DataStructure-Cy']
+    install_requires=['NlpToolkit-Dictionary-Cy', 'NlpToolkit-Corpus-Cy', 'NlpToolkit-DataStructure-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
